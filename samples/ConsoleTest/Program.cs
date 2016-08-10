849b51f101f8c8d8ff2816fe0a3487dcb5d42b2c
Toni Petrina <tpetrina@tpetrina.com>
2016-08-10 16:52:14 +0200

    Added console sample app

---
+++ samples/ConsoleTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Sancho.Client.Core;
+using Sancho.Client.Plugin.Jint;
+
+namespace ConsoleTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DoStuff();
+
+            Console.ReadKey();
+        }
+
+        async static void DoStuff()
+        {
+            Connection.ProtocolUrl = "http://sanchoprotocol-dev.azurewebsites.net/signalr";
+
+            var connection = new Connection("00000000-0000-0000-0000-000000000000");
+            connection.AddPlugin(new JintPlugin(connection, typeof(object).Assembly));
+            Console.WriteLine($"{connection.DeviceId} connecting!");
+
+            var status = await connection.ConnectAsync();
+            Console.WriteLine($"status: {status}");
+
+            connection.On<Message>("Receive", m =>
+            {
+                Console.WriteLine("Got message from server");
+            });
+
+            Console.ReadKey();
+
+            connection.Disconnect();
+        }
+    }
+}
