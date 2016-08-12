7669d90b0e1e351f7e01a3cb1eb25b2f6e6ca643
Toni Petrina <tpetrina@tpetrina.com>
2016-08-12 11:27:55 +0200

    Updated samples

--- samples/ConsoleTest/Program.cs
+++ samples/ConsoleTest/Program.cs
@@ -18,11 +18,17 @@ namespace ConsoleTest
             Connection.ProtocolUrl = "http://sanchoprotocol-dev.azurewebsites.net/signalr";
 
             var connection = new Connection("00000000-0000-0000-0000-000000000000");
-            connection.AddPlugin(new JintPlugin(connection, typeof(object).Assembly));
+            connection.AddPlugin(new JintPlugin(connection, assemblies: typeof(object).Assembly));
             Console.WriteLine($"{connection.DeviceId} connecting!");
 
             var status = await connection.ConnectAsync();
-            Console.WriteLine($"status: {status}");
+            Console.WriteLine($"connected: {status}");
+            Console.WriteLine($"transport: {connection.HubConnection.Transport}");
+
+            connection.HubConnection.TraceLevel = Microsoft.AspNet.SignalR.Client.TraceLevels.All;
+            connection.HubConnection.Error += HubConnection_Error;
+            connection.HubConnection.StateChanged += HubConnection_StateChanged;
+            connection.HubConnection.Closed += HubConnection_Closed;
 
             connection.On<Message>("Receive", m =>
             {
@@ -31,7 +37,26 @@ namespace ConsoleTest
 
             Console.ReadKey();
 
+            Console.WriteLine("Disconnecting...");
             connection.Disconnect();
+            Console.WriteLine("Disconnected");
+        }
+
+        static void HubConnection_Error(Exception obj)
+        => Write(ConsoleColor.Red, obj.ToString());
+
+        static void HubConnection_StateChanged(Microsoft.AspNet.SignalR.Client.StateChange obj)
+        => Write(ConsoleColor.Gray, obj.NewState.ToString());
+
+        static void HubConnection_Closed()
+        => Write(ConsoleColor.White, "Closed");
+
+        static void Write(ConsoleColor color, string text)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = oldColor;
         }
     }
 }
---
+++ samples/Forms/FormsSample/FormsSamplePage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using Sancho.Client.Core;
+using Sancho.Client.Plugin.Jint;
+using Xamarin.Forms;
+
+namespace FormsSample
+{
+    public partial class FormsSamplePage : ContentPage
+    {
+        Connection connection;
+
+        public FormsSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Connection.ProtocolUrl = "http://sanchoprotocol-dev.azurewebsites.net/signalr";
+
+            connection = new Connection("00000000-0000-0000-0000-000000000000");
+            var jint = new JintPlugin(connection,
+                                      HandleAction,
+                                      assemblies: typeof(Color).GetTypeInfo().Assembly
+                                     );
+
+            jint.Engine.SetValue("ContentRoot", ContentRoot);
+            jint.Engine.SetValue("Info", Info);
+            jint.Engine.SetValue("log", new Action<object>(o => Debug.WriteLine(o.ToString())));
+            connection.AddPlugin(jint);
+            WriteLine($"{connection.DeviceId} connecting!");
+
+            var status = await connection.ConnectAsync();
+            WriteLine($"connected: {status}");
+
+            //connection.On<Message>("Receive", m =>
+            //{
+            //    WriteLine(m.command);
+            //});
+        }
+
+        void HandleAction(Action obj)
+        {
+            Device.BeginInvokeOnMainThread(obj);
+        }
+
+        void WriteLine(string text)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Info.Text = text;
+            });
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            ContentRoot.BackgroundColor = Color.Red;
+            Info.Text = "hello";
+        }
+    }
+}
